8b68fe178771f2e112a6f60ee3c687f2f3f7b0f6
Yuxuan Shui <yshuiv7@gmail.com>
2020-08-19 11:22:23 +0100

    Add rounded-corners-exclude configuration option
    Allows the user to selectively disable rounded corners.

--- src/options.c
+++ src/options.c
@@ -120,6 +120,9 @@ static void usage(const char *argv0, int ret) {
 	    "--corner-radius value\n"
 	    "  Round the corners of windows. (defaults to 0)\n"
 	    "\n"
+	    "--rounded-corners-exclude condition\n"
+	    "  Exclude conditions for rounded corners.\n"
+	    "\n"
 	    "--mark-wmwin-focused\n"
 	    "  Try to detect WM windows and mark them as active.\n"
 	    "\n"
@@ -444,6 +447,7 @@ static const struct option longopts[] = {
     {"blur-strength", required_argument, NULL, 331},
     {"shadow-color", required_argument, NULL, 332},
     {"corner-radius", required_argument, NULL, 333},
+    {"rounded-corners-exclude", required_argument, NULL, 334},
     {"experimental-backends", no_argument, NULL, 733},
     {"monitor-repaint", no_argument, NULL, 800},
     {"diagnostics", no_argument, NULL, 801},
@@ -860,6 +864,10 @@ bool get_cfg(options_t *opt, int argc, char *const *argv, bool shadow_enable,
 			// --cornor-radius
 			opt->corner_radius = atoi(optarg);
 			break;
+        case 334:
+            // --rounded-corners-exclude
+            condlst_add(&opt->rounded_corners_blacklist, optarg);
+            break;
 		P_CASEBOOL(733, experimental_backends);
 		P_CASEBOOL(800, monitor_repaint);
 		case 801: opt->print_diagnostics = true; break;
